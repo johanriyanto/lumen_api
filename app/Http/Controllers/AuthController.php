8f7900da40a288c5c8aeba11902fa5c6f2af60b0
johanriyan <johanmuse@gmail.com>
2021-12-02 14:55:13 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function register(Request $request)
+    {
+        //validate incoming request 
+        $this->validate($request, [
+            'name' => 'required|string',
+            'username' => 'required|string|unique:users',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        try 
+        {
+            $user = new User;
+            $user->name = $request->input('name');
+            $user->username = $request->input('username');
+            $user->email = $request->input('email');
+            $user->password = app('hash')->make($request->input('password'));
+            $user->save();
+
+            return response()->json( [
+                        'entity' => 'users', 
+                        'action' => 'create', 
+                        'result' => 'success'
+            ], 201);
+
+        } 
+        catch (\Exception $e) 
+        {
+            return response()->json( [
+                       'entity' => 'users', 
+                       'action' => 'create', 
+                       'result' => 'failed'
+            ], 409);
+        }
+    }
+	
+     /**
+     * Get a JWT via given credentials.
+     *
+     * @param  Request  $request
+     * @return Response
+     */	 
+    public function login(Request $request)
+    {
+          //validate incoming request 
+        $this->validate($request, [
+            'email' => 'required|email|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only(['email', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {			
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+	
+     /**
+     * Get user details.
+     *
+     * @param  Request  $request
+     * @return Response
+     */	 	
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+}
